b9f719149e4ec65552c53fe906707fd674d2383a
Pombert-JF <jpombert@iit.edu>
2021-03-12 14:58:03 -0600

    code cleanup

--- trRosetta/fasta_oneliner.pl
+++ trRosetta/fasta_oneliner.pl
@@ -1,21 +1,23 @@
 #!/usr/bin/perl
 ## Pombert Lab 2020
-my $version = 0.2;
+my $version = '0.2a';
 my $name = 'fasta_oneliner.pl';
+my $updated = '12/03/2021';
 
 use strict; use warnings; use Getopt::Long qw(GetOptions); use File::Basename;
 
 ## Usage definition
 my $USAGE = <<"OPTIONS";
-NAME		$name
-VERSION		$version
+NAME		${name}
+VERSION		${version}
+UPDATED		${updated}
 SYNOPSIS	Convert FASTA sequences into single string FASTA
 
-COMMAND         $name -f *.fasta -o FASTA_OL
+COMMAND		${name} -f *.fasta -o FASTA_OL
 
 OPTIONS:
--f (--fasta)    FASTA files to convert
--o (--output)   Output folder
+-f (--fasta)	FASTA files to convert
+-o (--output)	Output folder
 OPTIONS
 die "\n$USAGE\n" unless @ARGV;
 
@@ -23,24 +25,24 @@ die "\n$USAGE\n" unless @ARGV;
 my @fasta;
 my $out;
 GetOptions(
-    'f|fasta=s@{1,}' => \@fasta,
-    'o|output=s' => \$out,
+	'f|fasta=s@{1,}' => \@fasta,
+	'o|output=s' => \$out,
 );
 
 if (!defined $out){$out = './';}
-unless (-d $out){mkdir $out;}
+unless (-d $out){mkdir ($out,0755) or die "Can't create folder $out: $!\n";}
 
 ## Converting fasta files
 while (my $fasta = shift@fasta){
-    open FASTA, "<", "$fasta";
-    my($name, $dir) = fileparse($fasta);
-    my $header; my $seq;
-    while (my $line = <FASTA>){
-        chomp $line;
-        if ($line =~ /^>/){$header = $line;}
-        else{$seq .= $line;}
-    }
-    open OUT, ">", "$out/$name";
-    print OUT "$header\n"."$seq\n";
-    close FASTA; close OUT;
+	open FASTA, "<", "$fasta";
+	my($name, $dir) = fileparse($fasta);
+	my $header; my $seq;
+	while (my $line = <FASTA>){
+		chomp $line;
+		if ($line =~ /^>/){$header = $line;}
+		else{$seq .= $line;}
+	}
+	open OUT, ">", "$out/$name";
+	print OUT "$header\n"."$seq\n";
+	close FASTA; close OUT;
 }
--- trRosetta/run_hhblits.pl
+++ trRosetta/run_hhblits.pl
@@ -1,40 +1,42 @@
 #!/usr/bin/perl
 ## Pombert Lab 2020
-my $version = 0.2;
+my $version = '0.2a';
 my $name = 'run_hhblits.pl';
+my $updated = '12/03/2021';
 
 use strict; use warnings; use Getopt::Long qw(GetOptions);
 my @command = @ARGV; ## Keeping track of command line for log
 
 ## Usage definition
 my $USAGE = <<"OPTIONS";
-NAME		$name
-VERSION		$version
+NAME		${name}
+VERSION		${version}
+UPDATED		${updated}
 SYNOPSIS	Runs hhblits on provided fasta files to create .a3m files
 REQUIREMENTS	HH-suite3 - https://github.com/soedinglab/hh-suite
-                UNICLUST database - https://uniclust.mmseqs.com/
+		UNICLUST database - https://uniclust.mmseqs.com/
 
-COMMAND         $name \\
-                   -t 10 \\
-                   -f FASTA_OL/ \\
-                   -o HHBLITS/ \\
-                   -d /media/Data_3/Uniclust/UniRef30_2020_06 \\
-                   -e 1e-40 1e-10 1e-03
+COMMAND		${name} \\
+		  -t 10 \\
+		  -f FASTA_OL/ \\
+		  -o HHBLITS/ \\
+		  -d /media/Data_3/Uniclust/UniRef30_2020_06 \\
+		  -e 1e-40 1e-10 1e-03
 
 OPTIONS:
--t (--threads)	    Number of threads to use [Default: 10]
--f (--fasta)	    Folder containing fasta files
--o (--output)	    Output folder
--d (--database)     Uniclust database to query
--v (--verbosity)    hhblits verbosity; 0, 1 or 2 [Default: 2]
+-t (--threads)		Number of threads to use [Default: 10]
+-f (--fasta)		Folder containing fasta files
+-o (--output)		Output folder
+-d (--database)		Uniclust database to query
+-v (--verbosity)	hhblits verbosity; 0, 1 or 2 [Default: 2]
 
 ## E-value options
--e (--evalues)      Desired evalue(s) to query independently
--s (--seq_it)       Iterates sequentially through evalues
--se (--seq_ev)      Evalues to iterate through sequentially [Default:
-                    1e-70 1e-60 1e-50 1e-40 1e-30 1e-20 1e-10 1e-08 1e-06 1e-04 1e+01 ]
--ne (--num_it)      # of hhblits iteration per evalue (-e) [Default: 3]
--ns (--num_sq)      # of hhblits iteration per sequential evalue (-s) [Default: 1] 
+-e (--evalues)		Desired evalue(s) to query independently
+-s (--seq_it)		Iterates sequentially through evalues
+-se (--seq_ev)		Evalues to iterate through sequentially [Default:
+			1e-70 1e-60 1e-50 1e-40 1e-30 1e-20 1e-10 1e-08 1e-06 1e-04 1e+01 ]
+-ne (--num_it)		# of hhblits iteration per evalue (-e) [Default: 3]
+-ns (--num_sq)		# of hhblits iteration per sequential evalue (-s) [Default: 1] 
 OPTIONS
 die "\n$USAGE\n" unless @ARGV;
 
@@ -51,25 +53,25 @@ my $nint = 3;
 my $nseq = 1;
 GetOptions(
 	't|threads=i' => \$threads,
-    'f|fasta=s' => \$dir,
-    'o|output=s' => \$out,
-    'd|database=s' => \$uniclust,
-    'v|verbosity=i' => \$verb,
-    'e|evalues=s@{1,}' => \@evalues,
-    's|seq_it'  => \$sqit,
-    'se|seq_ev=s@{1,}' => \@seqev,
-    'ne|num_it=i' => \$nint,
-    'ns|num_sq=i' => \$nseq
+	'f|fasta=s' => \$dir,
+	'o|output=s' => \$out,
+	'd|database=s' => \$uniclust,
+	'v|verbosity=i' => \$verb,
+	'e|evalues=s@{1,}' => \@evalues,
+	's|seq_it'  => \$sqit,
+	'se|seq_ev=s@{1,}' => \@seqev,
+	'ne|num_it=i' => \$nint,
+	'ns|num_sq=i' => \$nseq
 );
 
 ## Reading from folder
 if (!defined $out){$out = './';}
-unless (-d $out){mkdir $out;}
+unless (-d $out){mkdir ($out,0755) or die "Can't create folder $out: $!\n";}
 opendir (DIR, $dir) or die $!;
 my @fasta;
 while (my $fasta = readdir(DIR)){
 		if (($fasta eq '.') || ($fasta eq '..')){next;}
-        elsif ($fasta =~ /.hhr$/){next;} ## Skipping previous hhblits results, if any
+		elsif ($fasta =~ /.hhr$/){next;} ## Skipping previous hhblits results, if any
 		else{push (@fasta, $fasta);}
 }
 @fasta = sort@fasta;
@@ -80,26 +82,50 @@ open LOG, ">", "$out/hhblits.log";
 print LOG "COMMAND LINE:\n$name @command\n"."$name version = $version\n";
 print LOG "hhblits search(es) started on: $start\n";
 while (my $fasta = shift@fasta){
-    my ($prefix, $ext) = $fasta =~ /^(\S+)\.(\w+)$/;
-    ## Running independent searches per evalue
-    if (@evalues){
-        foreach my $eval (@evalues){
-            print "\nWorking on file $dir/$fasta with evalue $eval ...\n\n";
-            system "hhblits -cpu $threads -i $dir/$fasta -oa3m $out/$prefix.$eval.a3m -d $uniclust -e $eval -n $nint -v $verb";
-        }
-    }
-    ## Running iterative searches, from stricter to more permissive evalues
-    if ($sqit){
-        my @seqit = (1e-70, 1e-60, 1e-50, 1e-40, 1e-30, 1e-20, 1e-10, 1e-08, 1e-06, 1e-04, 1e+01);
-        if (@seqev){@seqit = @seqev;}
-        my $start = shift@seqit;
-        print "\nIterating on file $dir/$fasta with evalue $start ...\n\n";
-        system "hhblits -cpu $threads -i $dir/$fasta -oa3m $out/$prefix.sqit.a3m -d $uniclust -e $start -n $nseq -v $verb";
-        foreach my $eval (@seqit){
-            print "\nIterating on file $dir/$fasta with evalue $eval ...\n\n";
-            system "hhblits -cpu $threads -i $out/$prefix.sqit.a3m -oa3m $out/$prefix.sqit.a3m -d $uniclust -e $eval -n $nseq -v $verb";
-        }
-    }
+	my ($prefix, $ext) = $fasta =~ /^(\S+)\.(\w+)$/;
+	## Running independent searches per evalue
+	if (@evalues){
+		foreach my $eval (@evalues){
+			print "\nWorking on file $dir/$fasta with evalue $eval ...\n\n";
+			system "hhblits \\
+			  -cpu $threads \\
+			  -i $dir/$fasta \\
+			  -oa3m $out/$prefix.$eval.a3m \\
+			  -d $uniclust \\
+			  -e $eval \\
+			  -n $nint \\
+			  -v $verb";
+		}
+	}
+	## Running iterative searches, from stricter to more permissive evalues
+	if ($sqit){
+		
+		my @seqit = (1e-70, 1e-60, 1e-50, 1e-40, 1e-30, 1e-20, 1e-10, 1e-08, 1e-06, 1e-04, 1e+01);
+		if (@seqev){@seqit = @seqev;}
+		my $start = shift@seqit;
+		
+		print "\nIterating on file $dir/$fasta with evalue $start ...\n\n";
+		system "hhblits \\
+		  -cpu $threads \\
+		  -i $dir/$fasta \\
+		  -oa3m $out/$prefix.sqit.a3m \\
+		  -d $uniclust \\
+		  -e $start \\
+		  -n $nseq \\
+		  -v $verb";
+		
+		foreach my $eval (@seqit){
+			print "\nIterating on file $dir/$fasta with evalue $eval ...\n\n";
+			system "hhblits \\
+			  -cpu $threads \\
+			  -i $out/$prefix.sqit.a3m \\
+			  -oa3m $out/$prefix.sqit.a3m \\
+			  -d $uniclust \\
+			  -e $eval \\
+			  -n $nseq \\
+			  -v $verb";
+		}
+	}
 }
 my $mend = localtime();
 print LOG "hhblits search(es) completed on $mend\n\n";
