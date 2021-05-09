df2a1202cf0385170f7be9a01c76921b807df324
Alexander Julian <ajulian@hawk.iit.edu>
2021-05-08 22:37:31 -0500

    Comments and prinout log update

--- trRosetta/create_pdb.pl
+++ trRosetta/create_pdb.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 ## Pombert Lab 2020
-my $version = '0.2b';
+my $version = '0.2c';
 my $name = 'create_pdb.pl';
-my $updated = '2021-04-27';
+my $updated = '2021-05-08';
 
 use strict; use warnings; use Getopt::Long qw(GetOptions); use File::Basename; use threads; use threads::shared;
 
@@ -35,7 +35,7 @@ die "\n$USAGE\n" unless @ARGV;
 my @commands = @ARGV;
 
 ## Defining options
-my @npz;
+my $npz_dir;
 my $out = './';
 my $trosetta;
 my $fasta;
@@ -43,7 +43,7 @@ my $threads = 10;
 my $python = 'python';
 my $memory = 16;
 GetOptions(
-	'n|npz=s@{1,}' => \@npz,
+	'n|npz=s' => \$npz_dir,
 	'o|output=s' => \$out,
 	't|trosetta=s' => \$trosetta,
 	'f|fasta=s' => \$fasta,
@@ -51,6 +51,15 @@ GetOptions(
 	'p|python=s' => \$python,
 	'm|memory=i' => \$memory
 );
+
+## Load npz files into an array
+my @npz;
+opendir(DIR,$npz_dir) or die("Can't open $npz_dir: $!\n");
+while (my $file = readdir(DIR)){
+	if ($file =~ /^(\w+)\.npz/){
+		push(@npz,"$npz_dir/$file");
+	}
+}
 
 ## Checking output folder
 unless (-d $out){
@@ -70,24 +79,34 @@ my @threads = initThreads();
 ## Copying the array into a shared list for multithreading (use threads::shared;)
 my @files :shared = @npz;
 
-
+## Setting the maximum shared file size to prevent RAM overloading
 my $max_file_memory :shared  = 0.0078125*($memory*(10**9));
+## Printable version of shared file size
 my $max_file_memory_p = $max_file_memory/(10**6);
+## Setup shared file size tracker
 my $file_memory :shared = $max_file_memory;
+## Create large file array for single threading
 my @large_files :shared;
+## Total amount of files
 my $total_files :shared = scalar(@files);
+## Initialize running process printout
 my %running_processes :shared;
+## Running threads counter
 my $running_threads :shared = 0;
+## Folding threads counter
 my $folding_threads :shared = 0;
+## Threads completed
 my $completed :shared = 0;
 my $start :shared;
 my @output_pdb;
+## Printout buffer
 my $buffer = "-" x 100;
 
 ## Create threads that run the exe subroutine
 for my $thread (@threads){
 	$thread = threads -> create(\&mt_exe);
 }
+my $print_thread = threads -> create(\&mt_po);
 
 ## Run until threads are done
 for my $thread (@threads){
@@ -95,6 +114,8 @@ for my $thread (@threads){
 }
 
 $total_files = scalar(@large_files);
+
+$completed = 0;
 
 if (@large_files){
 	my $thr1 = threads -> create(\&st_exe);
@@ -115,6 +136,8 @@ sub initThreads{
 	return @initThreads;
 }
 
+
+### Multi-thread folding function
 sub mt_exe{
 
 	## Get the thread id. Allows each thread to be identified.
@@ -131,6 +154,8 @@ sub mt_exe{
 
 		my $npz;
 
+		## If the number of files is less than the number of threads, release non-utilized threads, if not, grab next
+		## npz file
 		unless (scalar(@files) > 0){
 			last PROCESS;
 		}
@@ -138,33 +163,9 @@ sub mt_exe{
 			lock(@files);
 			$npz = shift(@files);
 		}
-		
-		if ($0){
-			lock(@files);
-			lock(%running_processes);
-			lock($running_threads);
-			lock($file_memory);
-			system "clear";
-			my $remaining = "." x (int((scalar(@files)/$total_files)*100));
-			my $progress = "|" x (100-int((scalar(@files)/$total_files)*100));
-			my $status = "[".$progress.$remaining."]";
-			print "\nFolding Proteins with Multi-threading\n";
-			print "\n\t$status\t".($total_files-scalar(@files))."/$total_files";
-			print "\n\n\tThreads Running:\t$running_threads/$threads\n";
-			print "\tThreads Folding:\t$folding_threads/$threads\n";
-			print "\tAvailable Memory:\t".sprintf("%.2f",($file_memory/1000000))."/".($max_file_memory/1000000)." Mb\n\n\n";
-			print "Thread Status:\n$buffer\n";
-			foreach my $key (sort(keys(%running_processes))){
-				chomp($running_processes{$key});
-				print("$running_processes{$key}\n");
-			}
-			print("\n\n");
-		}
 
 		my ($name, $dir) = fileparse($npz);
 		my ($prefix, $evalue) = $name =~ /^(\S+)\.(\S+)\.(\w+)$/;
-
-		my $buffer = "-" x 100;
 
 		if (-e "$out/$prefix.$evalue.pdb"){
 			print LOG "$out/$prefix.$evalue.pdb already exists, moving to next npz...\n";
@@ -175,15 +176,15 @@ sub mt_exe{
 		if ((-s $npz) < $max_file_memory){
 			## Check if file can be opened given the alloted resources
 
+			## Update available file memory
 			if ($0){
-				## Remove file memory from memory available
 				lock($file_memory);
 				$file_memory -= -s $npz;
 			}
 
+			## If memory is available, fold the npz
 			if (0 < $file_memory){
-				## If file can be run, run it
-
+				## Update process printout
 				if($0){
 					lock(%running_processes);
 					lock($folding_threads);
@@ -191,13 +192,17 @@ sub mt_exe{
 					$folding_threads ++;
 				}
 
+				## Get starttime, run process, and get stop time
+				my $starttime = `date`;
 				system "$python \\
 					$trosetta \\
 					$npz \\
 					$fasta/$prefix.fasta \\
 					$out/$prefix.$evalue.pdb"
 				;
+				my $endtime = `date`;
 
+				## If the file did not fold, push it back into the queue to try again
 				unless(-e "$out/$prefix.$evalue.pdb"){
 					lock(%running_processes);
 					$running_processes{$id} = "Thread $id: Failed to fold $name. Placing back in the queue on ".localtime()."\n";
@@ -206,7 +211,7 @@ sub mt_exe{
 				else{
 					lock(%running_processes);
 					$running_processes{$id} = "Thread $id: Folding on $name has completed.\n";
-					print LOG "\n$buffer\nThread $id has completed on file $name\n$buffer\n\n";
+					print LOG "\n$buffer\nFile $name:\nStarted $starttime\nCompleted $endtime\n$buffer\n\n";
 				}
 
 				if ($0){
@@ -216,7 +221,7 @@ sub mt_exe{
 
 			}
 			else{
-				## If file can't be run, put it at the back of the line and try again later
+				## If the file is greater than 50% of available memory, place it into the single file queue
 				if ((-s $npz) > .5*$max_file_memory){
 					lock(%running_processes);
 					$running_processes{$id} = "Thread $id: $name is too large for Multi-threading. Sendt to Single-threaded queue on ".localtime()."\n";
@@ -244,15 +249,51 @@ sub mt_exe{
 			push(@large_files,$npz);
 
 		}
-		sleep(15);
+		sleep(5);
 	}
 	lock($running_threads);
 	lock(%running_processes);
+	lock($completed);
 	$running_threads--;
 	$running_processes{$id} = "Thread $id: No more jobs to run. Exited on ".localtime()."\n";
+	$completed++;
 	threads -> exit();
 }
 
+### Multi-thread printout
+sub mt_po{
+	WHILE: while (0==0){
+
+		if($completed == $threads){
+			last WHILE;
+		}
+
+		if ($0){
+			lock(@files);
+			lock(%running_processes);
+			lock($running_threads);
+			lock($file_memory);
+			system "clear";
+			my $remaining = "." x (int((scalar(@files)/$total_files)*100));
+			my $progress = "|" x (100-int((scalar(@files)/$total_files)*100));
+			my $status = "[".$progress.$remaining."]";
+			print "\nFolding Proteins with Multi-threading\n";
+			print "\n\t$status\t".($total_files-scalar(@files))."/$total_files";
+			print "\n\n\tThreads Running:\t$running_threads/$threads\n";
+			print "\tThreads Folding:\t$folding_threads/$threads\n";
+			print "\tAvailable Memory:\t".sprintf("%.2f",($file_memory/1000000))."/".($max_file_memory/1000000)." Mb\n\n\n";
+			print "Thread Status:\n$buffer\n";
+			foreach my $key (sort(keys(%running_processes))){
+				chomp($running_processes{$key});
+				print("$running_processes{$key}\n");
+			}
+			print("\n\n");
+			sleep(2);
+		}
+	}
+}
+
+### Single-thread folding
 sub st_exe{
 	while (my $npz = shift(@large_files)){
 
@@ -266,6 +307,8 @@ sub st_exe{
 			$start = localtime();
 		}
 
+		my $starttime = `date`;
+
 		system "$python \\
 			$trosetta \\
 			$npz \\
@@ -273,9 +316,14 @@ sub st_exe{
 			$out/$prefix.$evalue.pdb"
 		;
 
+		my $endtime = `date`;
+
 		unless(-e "$out/$prefix.$evalue.pdb"){
 			print LOG "$out/$prefix.$evalue.pdb";
 			print LOG "\n$buffer\nMain thread has failed to fold file $name\n$buffer\n\n";
+		}
+		else{
+			print LOG "\n$buffer\nFile $name:\nStarted $starttime\nCompleted $endtime\n$buffer\n\n";
 		}
 	}
 	lock($completed);
@@ -283,11 +331,14 @@ sub st_exe{
 	threads -> exit();
 }
 
+### Single-thread printout
 sub st_po{
 	WHILE: while (0 == 0){
-		if($completed == 0){
+
+		if($completed == 1){
 			last WHILE;
 		}
+
 		system "clear";
 		my $remaining = "." x (int((scalar(@large_files)/$total_files)*100));
 		my $progress = "|" x (100-int((scalar(@large_files)/$total_files)*100));
