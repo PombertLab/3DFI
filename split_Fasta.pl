7a4aa00ab22e90851289889d0f672ee3036d5e49
Alexander Julian <ajulian@hawk.iit.edu>
2021-06-04 22:15:59 -0400

    Added sliding window fasta splitting

--- split_Fasta.pl
+++ split_Fasta.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 ## Pombert Lab 2020
-my $version = '0.1a';
+my $version = '0.3';
 my $name = 'split_Fasta.pl';
-my $updated = '2021-04-06';
+my $updated = '2021-06-04';
 
 use strict; use warnings; use PerlIO::gzip; use Getopt::Long qw(GetOptions);
 
@@ -11,65 +11,109 @@ my $USAGE = <<"OPTIONS";
 NAME		${name}
 VERSION		${version}
 UPDATED		${updated}
-SYNOPSIS	Splits a multifasta file into separate files, one per sequence
+SYNOPSIS	Splits a multifasta file into separate files, one per sequence. Has the additional
+			functionality to subdivide fasta sequences into various segments using a sliding window. 
 		
 USAGE EXAMPLE	${name} \\
 		  -f file.fasta \\
 		  -o output_folder \\
-		  -e fasta
+		  -e fasta \\
+		  -w \\
+		  -s 150 \\
+		  -l 75
 
 OPTIONS:
 -f (--fasta)	FASTA input file (supports gzipped files)
--o (--output)	Output directory; defaults to file name prefix
+-o (--output)	Output directory [Default: Split_Fasta]
 -e (--ext)	Desired file extension [Default: fasta]
+-w (--window)	Split individual fasta sequences into fragments using sliding window [Default: off]
+-s (--size)	Size of the the sliding window [Default: 250 (aa)]
+-l (--overlap)	Sliding window overlap [Default: 100 (aa)]
 OPTIONS
 die "\n$USAGE\n" unless @ARGV;
 
 ## Defining options
 my $fasta;
-my $out;
+my $out = './Split_Fasta';
 my $ext = 'fasta';
+my $window;
+my $window_size = 250;
+my $window_overlap = 100;
 GetOptions(
 	'f|fasta=s' => \$fasta,
 	'o|output=s' => \$out,
-	'e|ext=s'	=> \$ext
+	'e|ext=s'	=> \$ext,
+	'w|window' => \$window,
+	's|size=i' => \$window_size,
+	'l|overlap=i' => \$window_overlap,
 );
 
 ## Creating output folder
-if (!defined $out){
-	if ($fasta =~ /.gz$/){ ($out) = $fasta =~ /^(.*?).\w+.gz$/; }
-	else{ ($out) = $fasta =~ /^(.*?).\w+$/; }
+unless (-d $out){
+	mkdir ($out,0755) or die "Can't create directory $out: $!\n";
 }
-if (-d $out){
-	print "Folder $out already exists... Do you want to continue? (y or n)\n";
-	my $answer = <STDIN>;
-	chomp $answer;
-	$answer = lc($answer);
-	if ($answer eq 'n'){ die "Stopping as requested\n"; }
-}
-else { mkdir ($out,0755) or die "Can't create folder $out: $!\n"; }
 
-## Working on multifasta file
+if($window){
+	unless (-d "$out/Windowed_Fastas"){
+		mkdir ("$out/Windowed_Fastas",0755) or die "Can't create directory $out/Windowed_Fastas: $!\n";
+	}
+}
+
+## Open multifasta file
 my $gzip = '';
 if ($fasta =~ /.gz$/){ $gzip = ':gzip'; }
 open FASTA, "<$gzip", "$fasta" or die "Can't open FASTA file $fasta: $!\n";
 
+## Storing fasta sequences under their locus
 my %sequences; my $seq;
 while (my $line = <FASTA>){
 	chomp $line;
 	if ($line =~ />(\S+)/){
 		$seq = $1;
-		print "$seq\n";
 	}
 	else { $sequences{$seq} .= $line; }
 }
-if ($gzip eq ':gzip'){ binmode FASTA, ":gzip(none)"; }
+if ($gzip eq ':gzip'){ 
+	binmode FASTA, ":gzip(none)"; 
+}
 
-## working on sequences
-for (keys %sequences){
-	open OUT, ">", "$out/$_.$ext" or die "Can't create FASTA file $fasta in folder $out: $!\n";
-	print OUT ">$_\n";
-	my @fsa = unpack ("(A60)*", $sequences{$_});
-	while (my $fsa = shift @fsa){ print OUT "$fsa\n"; }
+## Printing sequences
+for my $locus (keys(%sequences)){
+	open OUT, ">", "$out/$locus.$ext" or die "Can't open $locus.$ext in directory $out: $!\n";
+	print OUT ">$locus\n";
+	my $sequence = $sequences{$locus};
+	my @fsa = unpack ("(A60)*", $sequence);
+	while (my $fsa = shift @fsa){ 
+		print OUT "$fsa\n";
+	}
 	close OUT;
+	
+	## Creating windowed fasta files if specified
+	my $buffer = scalar(split("",length($sequence)));
+	if($window){
+		my $start = sprintf("%0${buffer}d",0);
+		my $remaining_aa = length($sequence);
+		ITER: while (0 == 0){
+			$remaining_aa -= ($window_size - $window_overlap);
+			my $end;
+			## Unless there are enough amino acids remaining after this subsequence for another windowed slice,
+			## extend the end point to the last amino acid in the sequence
+			unless ($remaining_aa > $window_size){
+				$end =  sprintf("%0${buffer}d",length($sequence)-1);
+			}
+			else{
+				$end = sprintf("%0${buffer}d", $start + $window_size);
+			}
+			my $subsequence = substr($sequence,$start,$end);
+			my @subfsa = unpack ("(A60)*",$subsequence);
+			open OUT, ">", "$out/Windowed_Fastas/${locus}_$start-$end.$ext" or die "Can't open ${locus}_$start-$end.$ext in directory $out/Windowed_Fastas: $!\n";
+			while (my $subfsa = shift(@subfsa)){
+				print OUT "$subfsa\n";
+			}
+			if ($end == length($sequence)-1){
+				last ITER;
+			}
+			$start += ($window_size - $window_overlap);
+		}
+	}
 }
