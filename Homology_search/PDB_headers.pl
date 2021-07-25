db0a0048d691075e4740fa967e4df7f34ac98360
Jean-Francois Pombert <jpombert@iit.edu>
2021-07-25 15:39:35 -0500

    added sprintf to minutes

--- Homology_search/PDB_headers.pl
+++ Homology_search/PDB_headers.pl
@@ -136,6 +136,7 @@ while (my $pb = shift@pdb){
 
 my $final_count = commify($pdb_count);
 my $run_time = (time - $start)/60;
+$run_time = sprintf ("%.2f", $run_time);
 print "\nIterated through a total of $final_count PDB files\n";
 print "Job completed in $run_time minutes.\n";
 
