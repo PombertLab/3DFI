6dd2df9aef4565ab7ad77a8b3cf07a62c179cccb
Jean-Francois Pombert <jpombert@iit.edu>
2021-08-17 09:30:54 -0500

    added PATH=$PATH: to config file

--- setup_3DFI.pl
+++ setup_3DFI.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 ## Pombert Lab, Illinois Tech, 2021
 my $name = 'setup_3DFI.pl';
-my $version = '0.2a';
-my $updated = '2021-08-04';
+my $version = '0.3';
+my $updated = '2021-08-17';
 
 use strict; use warnings; use Getopt::Long qw(GetOptions); use File::Basename; use Cwd qw(abs_path); 
 
@@ -46,7 +46,7 @@ if (exists $ENV{'HS_3DFI'}){  print "Env. variable \$HS_3DFI already set to $ENV
 if (exists $ENV{'VZ_3DFI'}){  print "Env. variable \$VZ_3DFI already set to $ENV{'VZ_3DFI'}\n"; $env_flag = 1; }
 
 if ($env_flag == 1){
-	print "\nOne or more 3DFI environment variables are already set. Do you wish to continue (y/n)?\n";
+	print "\nOne (or more) 3DFI environment variable(s) is already set. Do you wish to continue (y/n)?\n\n";
 	my $answer;
 	ANSWER: {
 		$answer = <STDIN>;
@@ -71,14 +71,15 @@ print "Configuration file to edit: $abs_path_config\n\n";
 print "\# Setting 3DFI environment variables as:\n";
 set_env(\*STDOUT);
 
-## Adding to config file
+## Adding environment variables and PATH to configuration file
 open CONFIG, ">>", "$abs_path_config" or die "Can't open $abs_path_config: $!\n";
-print CONFIG "\n".'### 3DFI environment variables'."\n";
 set_env(\*CONFIG);
+set_path(\*CONFIG);
 
 ## subroutine
 sub set_env {
 	my $fh = shift;
+	print $fh "\n".'### 3DFI environment variables'."\n";
 	print $fh "export TDFI=$abs_path_3DFI\n";
 	print $fh "export RX_3DFI=$abs_path_3DFI/Prediction/RaptorX\n";
 	print $fh "export TR_3DFI=$abs_path_3DFI/Prediction/trRosetta\n";
@@ -87,4 +88,16 @@ sub set_env {
 	print $fh "export RF_3DFI=$abs_path_3DFI/Prediction/RoseTTAFold\n";
 	print $fh "export HS_3DFI=$abs_path_3DFI/Homology_search\n";
 	print $fh "export VZ_3DFI=$abs_path_3DFI/Visualization\n\n";
+}
+sub set_path {
+	my $fh = shift;
+	print $fh "\n".'### 3DFI PATH variables'."\n";
+	print $fh "PATH=\$PATH:$abs_path_3DFI/Prediction/RaptorX\n";
+	print $fh "PATH=\$PATH:$abs_path_3DFI/Prediction/trRosetta\n";
+	print $fh "PATH=\$PATH:$abs_path_3DFI/Prediction/trRosetta2\n";
+	print $fh "PATH=\$PATH:$abs_path_3DFI/Prediction/AlphaFold2\n";
+	print $fh "PATH=\$PATH:$abs_path_3DFI/Prediction/RoseTTAFold\n";
+	print $fh "PATH=\$PATH:$abs_path_3DFI/Homology_search\n";
+	print $fh "PATH=\$PATH:$abs_path_3DFI/Visualization\n";
+	print $fh "\nexport PATH\n\n";
 }
