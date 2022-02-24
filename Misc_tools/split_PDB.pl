3213dd54aa5770faa14a9c30877b2adfbda68081
Alexander Thomas Julian <ajulian@hawk.iit.edu>
2022-02-24 15:13:07 -0600

    code minification

--- Misc_tools/split_PDB.pl
+++ Misc_tools/split_PDB.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 ## Pombert Lab 2020
-my $version = '0.3c';
+my $version = '0.4';
 my $name = 'split_PDB.pl';
 my $updated = '2022-02-24';
 
@@ -8,6 +8,7 @@ use strict;
 use warnings;
 use PerlIO::gzip;
 use File::Basename;
+use File::Path qw(make_path);
 use Getopt::Long qw(GetOptions);
 
 ## Usage definition
@@ -24,103 +25,72 @@ USAGE EXAMPLE	${name} \\
 
 OPTIONS:
 -p (--pdb)	PDB input file (supports gzipped files)
--o (--output)	Output directory. If blank, will create one folder per PDB file based on file prefix
+-o (--outdir)	Output directory [Default: ./]
 -e (--ext)	Desired file extension [Default: pdb]
 OPTIONS
 die "\n$USAGE\n" unless @ARGV;
 
 ## Defining options
 my @pdb;
-my $out;
+my $outdir = './';
 my $ext = 'pdb';
 GetOptions(
 	'p|pdb=s@{1,}' => \@pdb,
-	'o|output=s' => \$out,
+	'o|outdir=s' => \$outdir,
 	'e|ext=s'	=> \$ext
 );
 
+## Output dir
+unless (-d $outdir) {
+	make_path( $outdir, { mode => 0755 } )  or die "Can't create $outdir: $!\n";
+}
+
+## Working on PDB files
 while (my $pdb = shift@pdb){
+
 	my ($filename, $dir) = fileparse($pdb);
 	my ($prefix) = $filename =~ /^(\w+)/;
 
-	## Creating output folder
-	if (!defined $out){
-		unless (-d $prefix) {
-			mkdir ($prefix,0755) or die "Can't create folder $prefix: $!\n";
-		}
-	}
-	else {
-		unless (-d $out) {
-			mkdir ($out,0755) or die "Can't create folder $out: $!\n";
-		}
-	}
+	my $subdir = "$outdir/$prefix";
+	mkdir ("$subdir",0755) or die "Can't create folder $subdir: $!\n";
 
 	## Working on PDB file
 	my $gzip = '';
 	if ($pdb =~ /.gz$/){ $gzip = ':gzip'; }
 	open PDB, "<$gzip", "$pdb" or die "Can't open PDB file $pdb: $!\n";
 	
-	my %chains;
-	my $chain;
 	my @header;
-	my %ids;
-	my $molecule;
-	my $cpchain;
+	my $chain;
+	my %chains;
 	
 	while (my $line = <PDB>){
 		chomp $line;
 		if ($line =~ /^HEADER|TITLE|SOURCE|KEYWDS|EXPDTA|REVDAT|JRNL/){ push(@header, $line); }
-		elsif ($line =~ /^COMPND\s+\d+\s+(MOLECULE:\s.*)$/){ $molecule = $1; }
-		elsif ($line =~ /^COMPND\s+\d+\s+CHAIN:\s(.*);/){
-			my @chains = split(",", $1);
-			foreach (@chains){
-				$_ =~ s/ //g;
-				$cpchain = $_;
-				$ids{$cpchain} = $molecule;
-			}
-		}
-		elsif ($line =~ /^COMPND\s+\d+\s+CHAIN:\s(.*\w)/){
-			my @chains = split(",", $1);
-			foreach (@chains){
-				$_ =~ s/ //g;
-				$cpchain = $_;
-				$ids{$cpchain} = $molecule;
-			}
-		}
 		elsif ($line =~ /^ATOM\s+\d+\s+\S+\s+\w{3}\s(\S)/){
 			$chain = $1;
 			push (@{$chains{$chain}}, $line);
 		}
-		elsif($line =~ /^TER\s+\d+\s+\w{3}\s(\S)/){
-			$chain = $1;
+		elsif ($line =~ /^TER/){
 			push (@{$chains{$chain}}, $line);
 		}
+
 	}
 	if ($gzip eq ':gzip'){ binmode PDB, ":gzip(none)"; }
 
 	## Working on chains
-	for (keys %chains){
-		my $ch = $_;
-		if (defined $out){
-			open OUT, ">", "$out/${prefix}_$ch.$ext" or die "Can't create PDB file $pdb in folder $out: $!\n";
+	for my $ch (keys %chains){
+
+		open OUT, ">", "$subdir/${prefix}_$ch.$ext" or die "Can't create PDB file $pdb in folder $subdir: $!\n";
+
+		## write header
+		foreach (@header){
+			print OUT "$_\n";
 		}
-		else{
-			open OUT, ">", "$prefix/${prefix}_$ch.$ext" or die "Can't create PDB file $pdb in folder $out: $!\n";
-		}
-		foreach (@header){ print OUT "$_\n"; }
-		if (exists $ids{$ch}){
-			print OUT "COMPND    MOL_ID: 1;                                                            \n";
-			if ($ids{$ch} =~ /;/){
-				print OUT "COMPND   2 $ids{$ch}\n";
-			}
-			else{
-				chop $ids{$ch}; $ids{$ch} .= ';';
-				print OUT "COMPND   2 $ids{$ch}\n";
-			}
-			print OUT "COMPND   3 CHAIN: $ch;                                                            \n";
-		}
-		else{ print STDERR "Can't find DATA for $ch in $pdb\n"; }
-		while (my $line = shift @{$chains{$ch}}){ print OUT "$line\n"; }
+		my @core = @{$chains{$ch}};
+		for (0..$#core){
+			print OUT "$core[$_]\n";
+		} 
+
 		close OUT;
 	}
 }
