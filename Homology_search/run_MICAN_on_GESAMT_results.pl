2ac35880120f09d198d03133d7338dea4a0ab763
Jean-Francois Pombert <jpombert@iit.edu>
2022-04-27 15:11:17 -0500

    code cleanup

--- Homology_search/run_MICAN_on_GESAMT_results.pl
+++ Homology_search/run_MICAN_on_GESAMT_results.pl
@@ -2,8 +2,8 @@
 ## Pombert Lab 2022
 
 my $name = "run_MICAN_on_GESAMT_results.pl";
-my $version = "0.2";
-my $updated = "2022-03-15";
+my $version = "0.2a";
+my $updated = "2022-04-27";
 
 use strict;
 use warnings;
@@ -16,8 +16,8 @@ my $usage = <<"EXIT";
 NAME		${name}
 VERSION		${version}
 UPDATED		${updated}
-SYNOPSIS	The purpose of this script is to to calculate the Template Model (TM) score by running the MICAN structural alignment tool,
-		an indicator of the strength a predicted protein model shares with the expiremental model it is attempting to represent.
+SYNOPSIS	Calculates template model (TM) and RMSD scores with MICAN on structural
+		matches identified with foldseek or GESAMT
 
 USAGE
 
@@ -27,7 +27,7 @@ OPTION
 -o (--outdir)		Output directory (Default: MICAN_RESULTS)
 EXIT
 
-die("\n$usage\n") unless(@ARGV);
+die "\n$usage\n" unless @ARGV;
 
 my ($script,$pipeline_dir) = fileparse($0);
 $pipeline_dir =~ s/Homology_search/Misc_tools/;
@@ -65,34 +65,36 @@ if(-f "$outdir/MICAN_raw.tsv"){
 }
 
 my $datestring = localtime();
-open LOG, ">", "$outdir/MICAN.log" or die("Unable to create file $outdir/MICAN.log: $!\n");
-print LOG ("$0 \\\n-t $tdfi \\\n-r @rcsb \\\n-o $outdir\n\n");
-print LOG ("Started on $datestring\n");
+open LOG, ">", "$outdir/MICAN.log" or die "Unable to create file $outdir/MICAN.log: $!\n";
+print LOG "$0 \\\n-t $tdfi \\\n-r @rcsb \\\n-o $outdir\n\n";
+print LOG "Started on $datestring\n";
 
-open IN, "<", "$tdfi/Homology/GESAMT/All_GESAMT_matches_per_protein.tsv" or die "Unable to open file $tdfi/Homology/GESAMT/All_GESAMT_matches_per_protein.tsv: $!\n";
+my $tsv_name = "$tdfi/Homology/GESAMT/All_GESAMT_matches_per_protein.tsv";
+open IN, "<", $tsv_name or die "Unable to open $tsv_name: $!\n";
 my $total_alignments;
-while(my $line = <IN>){
-	unless(($line =~ /^###/) || ($line eq "")){
+while (my $line = <IN>){
+	unless (($line =~ /^###/) || ($line eq "")){
 		$total_alignments++;
 	}
 }
 close IN;
 
-open IN, "<", "$tdfi/Homology/GESAMT/All_GESAMT_matches_per_protein.tsv" or die "Unable to open file $tdfi/Homology/GESAMT/All_GESAMT_matches_per_protein.tsv: $!\n";
+open IN, "<", $tsv_name or die "Unable to open $tsv_name: $!\n";
 open RAW, ">", "$outdir/MICAN_raw.tsv";
-print RAW ("### Query\tPredictor\tRCSB Code\tChain\tsTMscore\tTMscore\tDali_Z\tSPscore\tLength\tRMSD\tSeq_Id\n");
+print RAW "### Query\tPredictor\tRCSB Code\tChain\tsTMscore\tTMscore\tDali_Z\tSPscore\tLength\tRMSD\tSeq_Id\n";
 
 my $alignment_counter = 0;
-while(my $line = <IN>){
-	chomp($line);
-	unless(($line =~ /^###/) || ($line eq "")){
+while (my $line = <IN>){
 
+	chomp($line);
+
+	unless (($line =~ /^###/) || ($line eq "")){
 
 		my ($query,$predictor,$rcsb_code,$chain,$qscore,$rmsd,$seq_id,$nAlign,$nRes,$rcsb_file,$annotation) = split("\t",$line);
 		my ($rcsb_sub_folder) = lc($rcsb_code) =~ /\w(\w{2})\w/;
 
 		## Checking to see if this calculation has been performed previously
-		if($resume{$query}{$predictor}{$rcsb_code}{$chain}){
+		if ($resume{$query}{$predictor}{$rcsb_code}{$chain}){
 			print($resume{$query}{$predictor}{$rcsb_code}{$chain}."\n");
 			next;
 		}
@@ -116,23 +118,14 @@ while(my $line = <IN>){
 
 			system "cp $rcsb_file_location $rcsb_temp_dir/$rcsb_file\n";
 
-			# split_PDB.pl \
-			#   -p files.pdb \
-			#   -o output_folder \
-			#   -e pdb
-
-			# -p (--pdb)	PDB input file (supports gzipped files)
-			# -o (--output)	Output directory. If blank, will create one folder per PDB file based on file prefix
-			# -e (--ext)	Desired file extension [Default: pdb]
-
-			if(-f "$pipeline_dir/split_PDB.pl"){
+			if (-f "$pipeline_dir/split_PDB.pl"){
 				system "$pipeline_dir/split_PDB.pl \\
 						-p $rcsb_temp_dir/$rcsb_file \\
 						-o $rcsb_temp_dir/tmp/ \\
 						-e pdb
 				";
 			}
-			else{
+			else {
 				print STDERR "[E] Cannot find $pipeline_dir/split_PDB.pl\n";
 			}
 
@@ -157,32 +150,32 @@ while(my $line = <IN>){
 			my $Seq_Id;
 			foreach my $line (@data){
 				chomp($line);
-				if($line =~ /Rank\s+sTMscore/){
+				if ($line =~ /Rank\s+sTMscore/){
 					$grab = 1;
 				}
-				if(($grab) && ($line =~ /^\s+(1.*)/)){
+				if (($grab) && ($line =~ /^\s+(1.*)/)){
 					undef($grab);
 					($rank,$sTMscore,$TMscore,$Dali_Z,$SPscore,$Length,$RMSD,$Seq_Id) = split(/\s+/,$1);
-					print RAW ("$query\t$predictor\t$rcsb_code\t$chain\t$sTMscore\t$TMscore\t$Dali_Z\t$SPscore\t$Length\t$RMSD\t$Seq_Id\n");
+					print RAW "$query\t$predictor\t$rcsb_code\t$chain\t$sTMscore\t$TMscore\t$Dali_Z\t$SPscore\t$Length\t$RMSD\t$Seq_Id\n";
 				}
 			}
 		}
-		else{
+		else {
 			print STDERR "Unable to find $rcsb_file\n";
 		}
 
 	}
-	elsif($line =~ /^### (\w+)/){
-		print RAW ("### $1\n");
+	elsif ($line =~ /^### (\w+)/){
+		print RAW "### $1\n";
 	}
 	else{
-		print RAW ("\n");
+		print RAW "\n";
 	}
 }
 
-print LOG ("$alignment_counter proteins aligned\n");
+print LOG "$alignment_counter proteins aligned\n";
 $datestring = localtime();
-print LOG ("Completed on $datestring\n");
+print LOG "Completed on $datestring\n";
 
 close IN;
 close RAW;
